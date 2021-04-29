581b4e5f55b7db1730a75ddbc5034c3a6466d5b4
omer <omer1miron@gmail.com>
2021-04-29 14:48:53 +0300

    Update main.py

--- SuccessRate/main.py
+++ SuccessRate/main.py
@@ -23,10 +23,6 @@ with open(path + "\..\Pickles\Ref.pickle", "rb") as p:
 log = open(path + '/../Example Files/Logs/%s.txt' % datetime.now().strftime('%d.%m.%y %H;%M;%S'), 'w')
 
 
-def reverse(input):
-    return input[::-1]
-
-
 def LOG(str):
     global log
     log.write(str)
@@ -45,17 +41,38 @@ def phase_comparison():
     return correct, not_correct
 
 
+def coverage(given_IRR, key, All_coverage_AS):
+    if IRR[key] != 'Unknown':
+        All_coverage_AS.append(key[0])
+        All_coverage_AS.append(key[1])
+
+
+def is_match(class1, class2):
+    return 1 if class1 == class2 else 0
+
+
+def key_analysis(given_dicts, key1, reverse=False):
+    dict1, dict2 = given_dicts
+    key2 = reversed(key1) if reverse else key1
+    if key1 not in dict1.keys() or key2 not in dict2.keys():
+        return 0, 0
+    if dict1[key1] == 'Unknown' or dict2[key2] == 'Unknown':
+        return 0, 0
+    match = (not reverse) * is_match(dict1[key1], dict2[key2])\
+        + reverse * is_match(dict1[key1], reversed(dict2[key2]))
+    return 1, match
+
+
 def IRR_analysis(given_IRR):
     global Ref
     matching_classifications = 0
-    conflicts = 0
-    agreements = 0
+    two_sided_classifications = 0
+    two_sided_agreements = 0
     unknown = list(given_IRR.values()).count('Unknown')
     number_of_ToRs_in_our_IRR = len(list(given_IRR.keys())) - unknown
     number_of_ToRs_in_ref_IRR = len(list(Ref.keys()))
-    number_of_ToRs_in_our_and_ref_IRR = len(set(list(Ref.keys()) + list(given_IRR.keys())))
     known_keys_in_both = 0
-    correct, not_correct = phase_comparison()
+    #correct, not_correct = phase_comparison()
     confusion_matrix = {
         'P2P': {
             'P2P': 0,
@@ -73,13 +90,25 @@ def IRR_analysis(given_IRR):
             'C2P': 0
         }
     }
+    # for key in given_IRR.keys():
+    #     if key[::-1] in given_IRR:
+    #         if given_IRR[key] == 'Unknown' or given_IRR[reversed(key)] == 'Unknown':
+    #             continue
+    #         if given_IRR[key] == reversed(given_IRR[reversed(key)]):
+    #             agreements += 1
+    #         else:
+    #             conflicts += 1
     for key in given_IRR.keys():
-        if key in Ref.keys():
-            known_keys_in_both += 1
-            if given_IRR[key] == Ref[key]:
-                matching_classifications += 1
-            if given_IRR[key] in ['P2P', 'P2C', 'C2P']:
-                confusion_matrix[Ref[key]][given_IRR[key]] += 1
+        key_in_both_dicts, key_match = key_analysis((given_IRR, Ref), (key, key))
+        known_keys_in_both += key_in_both_dicts
+        matching_classifications += key_match
+        key_2_sided, key_2_sided_match = key_analysis((given_IRR, given_IRR), (key, reversed(key)), True)
+        two_sided_classifications += key_2_sided
+        two_sided_agreements += key_2_sided_match
+        if not key_in_both_dicts: continue
+        confusion_matrix[Ref[key]][given_IRR[key]] += 1
+        coverage(given_IRR, key)
+    All_coverage_AS = list(set(All_coverage_AS))
     P2P = [confusion_matrix['P2P']['P2P'], confusion_matrix['P2P']['P2C'], confusion_matrix['P2P']['C2P']]
     P2C = [confusion_matrix['P2C']['P2P'], confusion_matrix['P2C']['P2C'], confusion_matrix['P2C']['C2P']]
     C2P = [confusion_matrix['C2P']['P2P'], confusion_matrix['C2P']['P2C'], confusion_matrix['C2P']['C2P']]
@@ -102,16 +131,8 @@ def IRR_analysis(given_IRR):
     LOG("\tNumber of Matching Classifications: %s\n" % matching_classifications)
     LOG("\tNumber of Known ToRs in this and CAIDA's IRR: %s\n" % known_keys_in_both)
     LOG("\tNumber of ToRs in Ref IRR: %s\n" % number_of_ToRs_in_ref_IRR)
-    for key in given_IRR.keys():
-        if key[::-1] in given_IRR:
-            if given_IRR[key] == 'Unknown' or given_IRR[reverse(key)] == 'Unknown':
-                continue
-            if given_IRR[key] == reverse(given_IRR[reverse(key)]):
-                agreements += 1
-            else:
-                conflicts += 1
-    LOG('\tnumber of 2 - sided agreements is %s\n' % agreements)
-    LOG('\tnumber of conflicts is %s\n' % conflicts)
+    LOG('\tratio of 2 - sided agreements is %s%%\n' % round(two_sided_agreements/two_sided_classifications, 2))
+    LOG('\tnumber of conflicts is %s%%\n' % round(1 - two_sided_agreements/two_sided_classifications, 2))
 
 
 def log_IRR(IRR, msg):
@@ -125,20 +146,17 @@ log_IRR(IRR2, 'Remarks Dictionary:')
 log_IRR(IRR3, 'Sets Dictionary:')
 log_IRR(IRR, 'Overall Dictionary:')
 log.close()
-with open(path + '/../Example Files/RefCompare.csv', mode='w', newline='') as f:
-    fwrite = csv.writer(f, delimiter=',')
-    fwrite.writerow(['AS1', 'AS2', 'IRR Prediction', 'Caida Prediction'])
-    for k in Ref.keys():
-        if k in IRR.keys():
-            w = [k[0][2:], k[1][2:], IRR[k], Ref[k], ' ']
-        else:
-            w = [k[0][2:], k[1][2:], "Doesn't Exist", Ref[k], ' ']
-        fwrite.writerow(w)
-    for k in (list(set(list(Ref.keys()) + list(IRR.keys())) - set(Ref.keys()))):
-        w = [k[0][2:], k[1][2:], IRR[k], "Doesn't Exist", ' ']
-        fwrite.writerow(w)
-
-
-
+# with open(path + '/../Example Files/RefCompare.csv', mode='w', newline='') as f:
+#     fwrite = csv.writer(f, delimiter=',')
+#     fwrite.writerow(['AS1', 'AS2', 'IRR Prediction', 'Caida Prediction'])
+#     for k in Ref.keys():
+#         if k in IRR.keys():
+#             w = [k[0][2:], k[1][2:], IRR[k], Ref[k], ' ']
+#         else:
+#             w = [k[0][2:], k[1][2:], "Doesn't Exist", Ref[k], ' ']
+#         fwrite.writerow(w)
+#     for k in (list(set(list(Ref.keys()) + list(IRR.keys())) - set(Ref.keys()))):
+#         w = [k[0][2:], k[1][2:], IRR[k], "Doesn't Exist", ' ']
+#         fwrite.writerow(w)
 
 
